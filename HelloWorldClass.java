e8aae40c63482f90fa355d2edc31fe253da10062
sehrast <saher.tabbasum@hive-worx.com>
2019-02-28 15:38:39 +0500

    files added

---
+++ HelloWorldClass.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello World..."+i);
+		}
+		System.out.println("My First Program.");
+	}
+
+}
